076850b1405b41ddeff6fc2967e32563500a166d
Julian Fisher <jfisher009@drury.edu>
2022-11-09 18:18:33 -0600

    Migrated to Quart

--- ReputableEntity/app.py
+++ ReputableEntity/app.py
@@ -1,15 +1,16 @@
 # File: api.py
 # Author: Julian Fisher
 # Date: 10/30/2022
-# Purpose: Define an API that will be used to enable 
-# reputable entities to communicate with each other 
+# Purpose: Define an API that will be used to enable
+# reputable entities to communicate with each other
 # and with the blockchain
 
-from flask import Flask
-from flask import request
+from quart import Quart
+from quart import request
 import requests
 import configparser
 import db
+import socket
 
 CONFIG_FILE = "config.ini"
 
@@ -19,14 +20,12 @@ config.read(CONFIG_FILE)
 tableName = config['sqlite']['receiptTableName']
 dbConn = db.DBConnection(config['sqlite']['dbName'])
 
-print(tableName)
-app = Flask(__name__)
+app = Quart(__name__)
 
 @app.route("/writeReceipt",methods=['POST'])
 def writeReceipt():
     receiptData = request.json
     q = f"INSERT INTO {tableName} (receipt) VALUES (\"{receiptData['receipt']}\");"
-    print(q)
     res = dbConn.query(q)
     if res == []:
         return "SAVED"
@@ -41,8 +40,25 @@ def receipt():
 
 @app.route("/publishReceipt",methods=['POST'])
 def publishReceipt():
-    return -1
+    # get a list of the addresses of our neighbors
+    neighborsFromConfig = config.items('RE-Connections')
+
+    reqIP = request.remote_addr
+    for neighbor in neighborsFromConfig:
+        # send a request to each neighbor
+        neighborIP = socket.gethostbyname(neighbor[1].split(":")[0])
+
+        # send a request to the neighbor if the incoming request was not from that neighbor
+        neighborRequests = []
+        if neighborIP != reqIP:
+            neighborRequests.append(grequests.post("http://" + neighborIP + "/publishReceipt"))
+        
+        res = grequests.map(neighborRequests)
+    return res
 
 @app.route("/verifyReceipt",methods=['POST'])
 def verifyReceipt():
     return -1
+
+if __name__ == "__main__":
+    app.run()
