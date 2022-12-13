065bda62852489de539fcc64adaec462f4a7d5cf
balvarez982 <60147088+balvarez982@users.noreply.github.com>
2022-12-13 04:27:04 -0600

    Add files via upload

---
+++ Tests/demo_test/3rd_party_subsystem.js
@@ -0,0 +1,124 @@
+const generate = require("./receipt_demo");
+
+function verify_receipt(receipt) {
+
+    console.log(receipt)
+
+    if(check_size(receipt) === true) {
+        console.log("Passed receipt size check.")
+        if(check_null(receipt) === true) {
+            console.log("Passed source and target verification")
+            if(check_claim(receipt.claim) === true) {
+                console.log("Passed claim verification.")
+                console.log("Receipt verified.")
+                return true
+            }
+            else {
+                console.log("Failed claim verification.")
+                return false
+            }
+        }
+        else {
+            console.log("Failed source and target verification")
+            return false
+        }
+    }
+    else {
+        console.log("Failed receipt size check.")
+        return false
+    }
+}
+
+verify_receipt(generate.random_receipt())
+
+function check_size(receipt) {
+    // Expected length is 3 for the size of a receipt
+    // Source, target, and claim lengths should be 1, 1, and 4 respectively
+    let size = Object.keys(receipt).length
+    let source_size = Object.keys(receipt.source).length
+    let target_size = Object.keys(receipt.target).length
+    let claim_size = Object.keys(receipt.claim).length
+
+    if(size != 3 || source_size != 1 || target_size != 1 || claim_size != 4) {
+        return false
+    }
+    else {
+        return true
+    }
+}
+
+function check_null(receipt) {
+    if(receipt.source.id != null && receipt.target.id != null) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function check_claim(claim) {
+    let types = ["Creation", "Modification", "Deletion"]
+    let categories = ["Rating", "Review"]
+    let valid_rating = [1, 2, 3, 4, 5]
+
+    if(claim.id != null) {
+        if(types.includes(claim.type) === true) {
+            if(claim.type === "Deletion") {
+                if(claim.category === null && claim.content === null) {
+                    return true
+                }
+                else {
+                    console.log("Invalid deletion request format.")
+                    return false
+                }
+            }
+            else {
+                if(categories.includes(claim.category)) {
+                    if(claim.content != null) {
+                        if(typeof claim.content === "string") {
+                            return true
+                        }
+                        else if(valid_rating.includes(claim.content)) {
+                            return true
+                        }
+                        else {
+                            console.log("Unknown claim content.")
+                            return false
+                        }
+                    }
+                }
+                else {
+                    console.log("Unknown claim category.")
+                    return false
+                }
+            }
+        }
+        else {
+            console.log("Improper claim category or type.")
+            return false
+        }
+    }
+    else {
+        console.log("Invalid claim id.")
+        return false
+    }
+
+}
+
+//quick dirty verification of verify_receipt method, comment out all console.logs with replace all except for the ones in the metthod, or don't.
+function test() {
+
+    let num_tests = 100
+    let check_passes = 0
+    let check_fails = 0
+    for(let i = 0; i < num_tests; i++) {
+        if(verify_receipt() === true) {
+            check_passes += 1
+        } 
+        else {
+            check_fails += 1
+        }
+    }
+    console.log("Tests Passed: " + check_passes)
+    console.log("Tests Failed: " + check_fails)
+}
---
+++ Tests/demo_test/receipt_demo.js
@@ -0,0 +1,89 @@
+const crypto = require("crypto");
+const fs = require("fs")
+const getRandomValues = require('get-random-values')
+
+function generate_key_pair() {
+    const {publicKey, privateKey} =
+    crypto.generateKeyPairSync("rsa", {
+        modulusLength: 2048,
+    });
+
+    const exported_pubkey_buffer = publicKey.export({
+        type: "pkcs1", 
+        format: "pem",
+    });
+    fs.writeFileSync("target_public.pem", exported_pubkey_buffer, {encoding: "utf-8"})
+
+    const exported_privkey_buffer = privateKey.export({
+        type: "pkcs1",
+        format: "pem",
+    });
+    fs.writeFileSync("target_private.pem", exported_privkey_buffer, {encoding: "utf-8"})
+}
+
+function random_receipt() {
+
+    let source_public_key = fs.readFileSync("source_public.pem", "utf-8")
+    let target_public_key = fs.readFileSync("target_public.pem", "utf-8")
+
+    source_public_key = source_public_key.replace(/\n/g, '')
+    target_public_key = target_public_key.replace(/\n/g, '')
+
+    let claim_id = uuid4()
+
+    temp = {
+        "source": {
+            "id": source_public_key
+        },
+        "target": {
+            "id": target_public_key
+        },
+        "claim": random_claim(claim_id)
+    }
+
+    return(temp)
+}
+
+function random_claim(claim_id) {
+    let types = ['Creation', 'Modification', 'Deletion']
+    let content_types = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi maximus felis quis dapibus pretium. Donec eget augue id leo mattis facilisis. Duis ut lacus ac orci mattis consectetur ac id tellus.', 'Vivamus leo velit, condimentum eu nunc eget, lacinia ultricies urna. Integer semper, libero eu dignissim luctus, turpis lacus fringilla sapien, et porttitor diam nunc vitae nunc.', 'Morbi aliquam elit eu urna accumsan ultricies. Phasellus non tellus eget magna pharetra vestibulum commodo eget est.']
+    var type = types[Math.floor(Math.random()*types.length)]
+    var choice = Math.floor(Math.random() * 2) + 1
+
+    if(type === 'Deletion') {
+        temp = {
+            "id": claim_id,
+            "type": type,
+            "category": null,
+            "content": null
+        }
+    }
+    else {
+        if(choice === 1) {
+            var category = "Review"
+            var content = content_types[Math.floor(Math.random()*content_types.length)]
+        }
+        else {
+            var category = "Rating"
+            var content = Math.floor(Math.random() * 5) + 1
+        }
+        temp = {
+            "id": claim_id,
+            "type": type,
+            "category": category,
+            "content": content
+        }
+    }
+
+    return(temp)
+}
+
+function uuid4() {
+    return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    );
+}
+
+module.exports = {
+    random_receipt
+}
